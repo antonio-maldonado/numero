dec79eb0e1ac997360add99a3499533813eb6b2c
Antonio Maldonado <artorio.13.45@gmail.com>
2023-08-15 10:11:39 -0600

    feat: boton reiniciar

--- js01.js
+++ js01.js
@@ -1,16 +1,7 @@
-const adivinanza = document.getElementById("num");
-const respuesta= document.getElementById("respuesta");
-let max = 100;
-let min = 1;
-let low = min, high = max;
-let numero = 1;
-
-numero = parseInt( low + (high - low)/2 );
-
-let adivinado = false;
-
 function menor(){
     if(adivinado) return;
+    adivinanza = document.getElementById("num");
+    respuesta= document.getElementById("respuesta");
     numero = parseInt( document.getElementById("num").textContent );
     high = numero - 1;
     low = min;
@@ -21,6 +12,8 @@ function menor(){
 
 function mayor(){
     if(adivinado) return;
+    adivinanza = document.getElementById("num");
+    respuesta= document.getElementById("respuesta");
     numero = parseInt( document.getElementById("num").textContent );
     low = numero + 1;
     high = max;
@@ -34,5 +27,30 @@ function correcto(){
     respuesta.textContent = "Tu número es el "+numero+" excelente decision";
     adivinado = true;
 }
+
+function reiniciar(){
+    adivinanza = document.getElementById("num");
+    respuesta= document.getElementById("respuesta");
+    max = 100;
+    min = 1;
+    low = min;
+    high = max;
+    numero = 1;
+    numero = parseInt( low + parseInt((high - low)/2) );
+    adivinado = false;
+    
+    respuesta.innerHTML = "¿Tu número es: <span id='num'>"+numero+"</span>?";
+}
+
+let adivinanza = document.getElementById("num");
+let respuesta= document.getElementById("respuesta");
+let max = 100;
+let min = 1;
+let low = min, high = max;
+let numero = 1;
+
+numero = parseInt( low + (high - low)/2 );
+
+let adivinado = false;
 
 adivinanza.textContent = numero ;
